4a1eb630e90d2e9c78a463013b1b5b39ba65fc27
sksdm <ssss.com>
2023-07-20 11:20:38 -0700

    s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * prskldint_numbers - print 0-9
+ *
  * return: void
  */
 void print_numbers(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
-		if(c!='2'||c!='4')
-
+		if (!(c == '2' || c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
+
 }
