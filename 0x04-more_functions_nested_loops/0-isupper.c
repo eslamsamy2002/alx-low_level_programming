b4c836a4998a129dd0c1a2e9392967ccac94846c
sksdm <ssss.com>
2023-07-20 10:01:28 -0700

    s

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,21 @@
+
 #include "main.h"
 /**
- * _isupper - check if c is upper
- *
- * @c: input for alphapet 
- *
- * Return : 1 if tis uppercase, 0 if not 
- */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		return(1);
-	else
-	       return (0);	
-}	
+
+
+  if (c >= 'A' && c <= 'Z')
+  {
+         return (1);
+  }
+  else
+  {
+          return (0);
+  }
+
+}
