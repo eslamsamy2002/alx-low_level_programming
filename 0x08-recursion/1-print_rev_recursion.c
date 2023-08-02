ab7e23d2043ad7c0c4ec30b0cb9102af12d2cd62
sksdm <ssss.com>
2023-08-02 16:12:44 -0400

    s

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,10 +3,10 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)
+void _print_rev_recursion (char *s)	
 {
 	if(*s)
-	{
+	{	
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}	
