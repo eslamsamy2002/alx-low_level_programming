2037cbed97215b6910dcd024f1faf27795b4d017
eslam <eslam@org.com>
2023-09-04 16:54:08 -0400

    list

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,6 +10,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf;
 	ssize_t w;
 	ssize_t t;
 
@@ -17,7 +18,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (fd == -1)
 		return (0);
-	char *buf = malloc(sizeof(char) * letters);
+
+	buf = malloc(sizeof(char) * letters);
 
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
