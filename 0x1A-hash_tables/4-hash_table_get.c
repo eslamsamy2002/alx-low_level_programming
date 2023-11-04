ed8b402ec6f9e0ccfc8895009bb0657c0793b0a7
eslam <eslam@org.com>
2023-11-03 20:18:54 -0400

    mm

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,35 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - get value of a key in a hash table
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
  *
- * @ht: pointer to the hash table
- * @key: key of the node to update
- *
- * Return: string value on success, NULL on failure
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int k_index;
-	hash_node_t *current;
-	/*check validation*/
-	if (ht == NULL || key == NULL || key[0] == '\0' || ht->size == 0)
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	/*get index*/
-	k_index = key_index((unsigned char *)key, ht->size);
-
-	if (ht->array[k_index] == NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
 		return (NULL);
-	current = ht->array[k_index];
 
-	while (current)
-	{
-		if (strcmp(current->key, key) == 0)
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-			return (current->value);
-
-		current = current->next;
-	}
-	return (NULL);
+	return ((node == NULL) ? NULL : node->value);
 }
+
