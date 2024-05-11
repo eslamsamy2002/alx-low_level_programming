b7f6d3b6a9c692eb9efe10f9f0b6267eaa5aa658
eslam <eslam@org.com>
2024-05-11 16:03:19 -0400

    mm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,11 +13,11 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-		return -1;
-	for (i = 0; i < size; i++ )
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		printf("Found %ld at index: %d\n", i, array[i]);
-		if(array[i] == value)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 		{
 			return (i);
 		}
